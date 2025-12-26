93705ac1d4081b64a05614896f9c5e04ba009ab3
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2025-12-27 01:24:12 +0600

    Java

---
+++ Variable.java
@@ -0,0 +1,12 @@
+public class Variable {
+    public static void main(String[] args) {
+        double number = 10;
+        String text = "Hello, Java!";
+        float decimal = 5.5f;
+
+        System.out.println("Integer: " + number);
+        System.out.println("String: " + text);
+        System.out.println("Float: " + decimal);
+    }
+    
+}
