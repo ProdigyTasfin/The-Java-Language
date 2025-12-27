ec282dc64915328fce965fe81ed4d12940fc73a8
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2025-12-27 20:47:56 +0600

    newLine

--- New_line.java
+++ New_line.java
@@ -2,5 +2,6 @@ public class New_line {
     public static void main(String[] args) {
         System.out.println("This is the first line. using println");
         System.out.print("This is the second line. using backslash n \n");
+        System.out.printf("This is the third line. using printf with newline character. %n");
     }
 }
