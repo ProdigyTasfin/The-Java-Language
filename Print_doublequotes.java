ddc6c9f58ab83be7e075165f099d6a5f9aa4e9b1
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2025-12-27 21:11:09 +0600

    Java

---
+++ Print_doublequotes.java
@@ -0,0 +1,7 @@
+public class Print_doublequotes {
+    public static void main(String[] args) {
+        System.out.println("She said, \"Hello, World!\"");
+        System.out.print("This is how to print a double quote: \"\n");
+        System.out.printf("Using printf to print double quotes: \"%s\"\n", "Example");
+    }
+}
--- Variable.java
+++ Variable.java
@@ -1,12 +1,21 @@
 public class Variable {
     public static void main(String[] args) {
-        double number = 10;
-        String text = "Hello, Java!";
-        float decimal = 5.5f;
+        int number = 10;
+        String text = "I'm learning variables!";
+        float decimal = 5.52f; // 'f' suffix indicates a float literal
+        byte b = 127; // byte range is -128 to 127, this will cause a compilation error
+        short s = 10000; // short range is -32,768 to 32,767, this will cause a compilation error
+        boolean flag = true; 
+        double pi = 3.14159; // double type for more precision
+        long bigNumber = 100000066789L; // 'L' suffix indicates a long literal
 
         System.out.println("Integer: " + number);
         System.out.println("String: " + text);
         System.out.println("Float: " + decimal);
+        System.out.println("Byte: " + b);
+        System.out.println("Short: " + s);
+        System.out.println("Boolean: " + flag);
+        System.out.println("Long: " + bigNumber);
     }
     
 }
