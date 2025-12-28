d78735b1d47e500d4175b658d37c77c07eeb8168
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2025-12-28 21:12:53 +0600

    input

--- Input.java
+++ Input.java
@@ -13,20 +13,20 @@ System.out.println("How old are you?");
 Scanner input = new Scanner(System.in);
 double age = input.nextDouble();
 System.out.println(name + ", your age is " + age + " years.");
-input.nextLine();
+// input.next();
 
 System.out.println("What is your favorite subject?");
-String subject = input.nextLine();
+String subject = input.next();
 System.out.println(name + ", your fav's subject is " + subject);
 
 System.out.println("Tell me! your height in centimeters?");
 double height = input.nextDouble();
 System.out.print(name + ", your height is " + height + " cm.\n");
 
-input.nextLine(); // Consume the leftover newline
+// input.nextLine(); // Consume the leftover newline
 
 System.out.println("Tell me! your fav movie");
-String movie = input.nextLine();
+String movie = input.next();
 System.out.println(name + ", your fav movie is " + movie);
 
 
