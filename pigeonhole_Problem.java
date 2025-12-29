3c96e8d17b5ad5f9b53b1598d701d2e89599b372
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2025-12-29 22:59:28 +0600

    pigeonhole

---
+++ pigeonhole_Problem.java
@@ -0,0 +1,21 @@
+// There is 12 pigeons and 11 pigeonholes.
+// Prove that at least one pigeonhole must contain more than one pigeon.
+// user-based solution using the pigeonhole principle.
+
+import java.util.Scanner;
+
+public class pigeonhole_Problem {
+
+    public static void main(String[] args){
+    
+    double x,y;
+    System.out.print("Number of holes: ");
+    Scanner input = new Scanner(System.in);
+    x = input.nextDouble();
+    System.out.print("Number of pigeons: ");
+    y = input.nextDouble();
+    double z = Math.ceil(y/x);
+    System.out.print("Number of pigeonholes: "+z);
+    }
+    
+}
