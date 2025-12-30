108d853b0fd524bea02448ee0674e730baed53ad
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2025-12-30 15:34:25 +0600

    Project

--- Project.java
+++ Project.java
@@ -11,10 +11,15 @@ public class Project {
         int random_Number = new Random().nextInt(100);
         int user_guess;
         int number_of_guesses;
+        int points = 100;
+
         Scanner input = new Scanner(System.in);
+
         System.out.println("Welcome to the Number Guessing Game!");
+
         System.out.println("I have selected a number between 0 and 100. Can you guess it?");
-        for(number_of_guesses = 0; ; number_of_guesses++){
+
+        for(number_of_guesses = 0; ; number_of_guesses++, points -= 5){
             System.out.print("Enter your guess: ");
             user_guess = input.nextInt();
             if(user_guess < random_Number){
@@ -23,6 +28,7 @@ public class Project {
                 System.out.println("Too high! Try again.");
             } else {
                 System.out.println("Congratulations! You've guessed the number " + random_Number + " in " + number_of_guesses + " attempts.");
+                System.out.println("Your points: " + points);
                 break;
             }
         }
