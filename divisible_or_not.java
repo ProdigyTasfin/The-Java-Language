55e2063058bc843da52d6f8737d19899b6cfd6fc
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2025-12-30 14:44:58 +0600

    divisible

---
+++ divisible_or_not.java
@@ -0,0 +1,10 @@
+public class divisible_or_not {
+    public static void main(String[] args) {
+        int number = 10; // You can change this number to test
+        if (number % 2 == 0) {
+            System.out.println(number + " is divisible by 2.");
+        } else {
+            System.out.println(number + " is not divisible by 2.");
+        }
+    }
+}
