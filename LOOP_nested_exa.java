d12efda8d2c71b40a01e49eb6762e4d4a638a369
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2025-12-31 21:44:54 +0600

    loop

--- LOOP_nested_exa.java
+++ LOOP_nested_exa.java
@@ -1,8 +1,8 @@
 public class LOOP_nested_exa {
     public static void main(String[] args) {
-        for (int i = 1; i <= 3; i++) {
+        for (int i = 1; i <= 10; i++) {
             System.out.println("Table of " + i + ":");
-            for (int j = 1; j <= 5; j++) {
+            for (int j = 1; j <= 10; j++) {
                 System.out.println(i + " x " + j + " = " + (i * j));
             }
             System.out.println();
