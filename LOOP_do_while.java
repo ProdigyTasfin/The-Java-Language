9adcbb53223319507a1119ff35d3f773e291f09e
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2025-12-31 21:34:11 +0600

    loop

---
+++ LOOP_do_while.java
@@ -0,0 +1,9 @@
+public class LOOP_do_while {
+    public static void main(String[] args) {
+        int count = 1;
+        do {
+            System.out.println("Count is: " + count);
+            count++;
+        } while (count <= 5);
+    }
+}
---
+++ LOOP_do_while_exa.java
@@ -0,0 +1,11 @@
+public class LOOP_do_while_exa {
+    public static void main(String[] args) {
+        int number = 10;
+        int factorial = 1;
+        do {
+            factorial *= number;
+            number--;
+        } while (number > 0);
+        System.out.println("Factorial is: " + factorial);
+    }
+}
