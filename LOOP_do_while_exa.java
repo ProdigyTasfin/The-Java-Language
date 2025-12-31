b56ee0cfa01beb68c9fa169143887ac6bb3ccf1b
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2025-12-31 21:36:25 +0600

    loop

--- LOOP_do_while_exa.java
+++ LOOP_do_while_exa.java
@@ -1,6 +1,6 @@
 public class LOOP_do_while_exa {
     public static void main(String[] args) {
-        int number = 10;
+        int number = 3;
         int factorial = 1;
         do {
             factorial *= number;
