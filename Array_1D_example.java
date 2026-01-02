910b15ead620e86981f30d67f1ea4f606ac665a1
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-01-02 22:00:13 +0600

    Array

--- Array_1D_example.java
+++ Array_1D_example.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class Array_1D_example {
-    public
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = input.nextInt();
+        int[] numbers = new int[size];
+        System.out.print("Enter the elements of the array: ");
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = input.nextInt();
+        }
+        System.out.println("The elements of the array are:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
 }
