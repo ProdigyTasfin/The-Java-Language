a104ce4cdeca1ec52db785442da997b9a9bd110d
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-01-03 22:20:12 +0600

    Array_List

--- Array_List.java
+++ Array_List.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+
 public class Array_List {
     public static void main(String[] args){
         ArrayList<String> fruits = new ArrayList<>();
         fruits.add("Apple");
         fruits.add("Banana");
         fruits.add("Cherry");
+        
         fruits.set(1, "Mango"); // Update Banana to Mango
         fruits.remove(0); // Remove Apple
         System.out.println("Fruits in the list:");
@@ -12,3 +15,4 @@ public class Array_List {
             
         }
     }
+}
