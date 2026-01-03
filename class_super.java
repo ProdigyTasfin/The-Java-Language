426add5814bdfd4d1a43a1099bba80feb7f92c2f
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-01-03 22:13:43 +0600

    class

---
+++ class_super.java
@@ -0,0 +1,10 @@
+//superclass 
+public class class_super {
+    public void display() {
+        System.out.println("This is the superclass method.");
+    }
+    public static void main(String[] args) {
+        class_super obj = new class_super();
+        obj.display();
+    }
+}
