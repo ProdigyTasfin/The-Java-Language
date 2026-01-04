c880fb95b900013ed2c81dcc016512db92b711e1
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-01-04 22:53:54 +0600

    method

---
+++ method_02.java
@@ -0,0 +1,11 @@
+public class method_02 {
+
+     static void main(String[] args){
+        String name = "Tasfin";
+        greet(name);
+     }
+
+    static void greet(String name){
+        System.out.print("Hello " + name);
+    }
+}
