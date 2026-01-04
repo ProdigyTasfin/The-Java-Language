7903f359af9ef72c129816d703a1dbe9e72449e7
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-01-04 23:12:56 +0600

    method

---
+++ method_03.java
@@ -0,0 +1,14 @@
+public class method_03 {
+
+    public static void main(String[] args) {
+
+        int x = 12;
+        int y = 21;
+        int result = sum(x, y); 
+        System.out.print("Result: " + result);  
+    }
+    
+    static int sum(int x, int y){
+        return x + y;
+    }
+}
