5304af3c39512e0881ab3e33abdf49359f55e8e1
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-01-04 22:32:43 +0600

    method

---
+++ method_01.java
@@ -0,0 +1,11 @@
+public class method_01 {
+    public static void main(String[] args) {
+        greet();
+        greet();
+        greet();
+    }
+
+    static void greet() {
+        System.out.println("Hello, welcome to Java programming!");
+    }
+}
