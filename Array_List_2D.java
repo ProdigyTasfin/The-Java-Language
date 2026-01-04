e6e932b756307bd15c5b98f67698e056d8f4e6db
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-01-04 19:48:49 +0600

    Array List

--- Array_List_2D.java
+++ Array_List_2D.java
@@ -1,20 +1,20 @@
-import java.util.*; 
+import java.util.*; // Importing the entire java.util package
 
 public class Array_List_2D {
     
     public static void main(String[] args) {
 
-        ArrayList<ArrayList<String>> groceryList = new ArrayList();
+        ArrayList<ArrayList<String>> groceryList = new ArrayList(); // 2D ArrayList
 
-        ArrayList<String> bakeryList = new ArrayList();
+        ArrayList<String> bakeryList = new ArrayList(); // 1D ArrayList
 
         bakeryList.add("Bread");
-        bakeryList.add("Croissant");
+        bakeryList.add("Croissant"); // Adding items to bakeryList
         bakeryList.add("Muffin");
 
         System.out.println("Bakery List: " + bakeryList);
 
-        ArrayList<String> productList = new ArrayList();
+        ArrayList<String> productList = new ArrayList(); // Another 1D ArrayList
 
         productList.add("Milk");
         productList.add("Eggs");
