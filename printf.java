74745a9e4baa20da5f5fb454abf4d2f8a0ee41ba
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-01-05 20:18:16 +0600

    printf

--- printf.java
+++ printf.java
@@ -4,8 +4,18 @@ public class printf {
 
         int x = 12;
         double y = 21;
-        System.out.printf("This is printing the value %.2f \n", y);
-        System.out.printf("This is printing the value %d", x);
+        String z = "Sakib";
+        boolean a = true;
+        char b = 'v';
+        float c = (float) 3.1416; 
+
+        System.out.printf("This is printing the int value %.2f \n", y);
+        System.out.printf("This is printing the double value %d \n", x);
+        System.out.printf("This is printing the string %s \n", z);
+        System.out.printf("This is printing the boolean %b \n", a);
+        System.out.printf("This is printing the character %c \n", b);
+        System.out.printf("This is printing the float value %.4f \n", c);
+
 
 
     }
