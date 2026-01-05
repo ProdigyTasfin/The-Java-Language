9d6035754cb5eac61f35a0e45218b0ebd1a7683d
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-01-05 21:12:04 +0600

    final keyword

---
+++ final_keyword.java
@@ -0,0 +1,8 @@
+public class final_keyword {
+    public static void main (String[] args){
+
+        final double pi = 3.1416;
+        // pi = 4; // you can't change it, because it already final
+        System.err.println(pi);
+    }
+}
--- printf.java
+++ printf.java
@@ -10,15 +10,15 @@ public class printf {
         float c = (float) 3.1416; 
         String name = "Sakib";
 
-        System.out.printf("This is printing the int value %.2f \n", y);
-        System.out.printf("This is printing the double value %d \n", x);
+        System.out.printf("This is printing the int value %d \n", x);
+        System.out.printf("This is printing the double value %.2f \n", y);
         System.out.printf("This is printing the string %s \n", z);
         System.out.printf("This is printing the boolean %b \n", a);
         System.out.printf("This is printing the character %c \n", b);
         System.out.printf("This is printing the float value %.4f \n", c);
         
         //Width
-        System.out.printf("Hello %10s", name);
+        System.out.printf("Hello %15s", name);
 
     }
 }
