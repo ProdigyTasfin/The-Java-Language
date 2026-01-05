9df0b43655374ca2cc026d9e24355bb35cfcb180
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-01-05 19:18:03 +0600

    method

--- method_overload.java
+++ method_overload.java
@@ -1,21 +1,42 @@
 public class method_overload {
-    public static void main (String[] args){
+    public static void main (String[] args)
+    {
 
         int x = add(1,2, 3,4);
+        double y = add(1.0,2.0, 3.0,4.0);
 
-        System.out.print("Result: " + x);
+        System.out.println("Result(int): " + x);
+        System.out.println("Result(double): " + y);
     }
 
-    static int add(int a, int b){
+    static int add(int a, int b)
+    {
         System.out.println("This is overload method #1");
-        return a + b;
+        return a + b; 
     }
-    static int add(int a, int b, int c){
+    static int add(int a, int b, int c)
+    {
         System.out.println("This is overload method #2");
         return a + b + c;
     }
-    static int add(int a, int b, int c, int d){
+    static int add(int a, int b, int c, int d)
+    {
         System.out.println("This is overload method #3");
+        return a + b + c + d;
+    }
+    static double add(double a, double b)
+    {
+        System.out.println("This is overload method #4");
+        return a + b; 
+    }
+    static double add(double a, double b, double c)
+    {
+        System.out.println("This is overload method #45");
+        return a + b + c;
+    }
+    static double add(double a, double b, double c, double d)
+    {
+        System.out.println("This is overload method #6");
         return a + b + c + d;
     }
 }
