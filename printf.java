cde6fb5bb5634339e90a8ce24c56e2e4fc48e30b
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-01-05 20:00:47 +0600

    printf

---
+++ printf.java
@@ -0,0 +1,12 @@
+public class printf {
+
+    public static void main (String[] args) {
+
+        int x = 12;
+        double y = 21;
+        System.out.printf("This is printing the value %.2f \n", y);
+        System.out.printf("This is printing the value %d", x);
+
+
+    }
+}
