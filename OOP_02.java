446b834a3107a8d9557114d8d6e1bf32bddde2ba
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-01-06 23:26:44 +0600

    OOP

--- OOP_02.java
+++ OOP_02.java
@@ -2,12 +2,12 @@
 
 public class OOP_02 {
     public static void main(String[] args) {
-        Pizza pizza = new Pizza("thicc crust", "tomato", "Mozerella", "Pineapples");
+        Pizza pizza = new Pizza("thick", "tomato", "mozzarella", "pepperoni"); // creating pizza object using constructor with 4 parameters
         System.out.println("The incredients of your pizza: ");
         System.out.println(pizza.bread);
-        System.out.println(pizza.sauce);
+        System.out.println(pizza.sauce); // accessing attributes of pizza object
         System.out.println(pizza.cheese);
-        System.out.println(pizza.toppings);
+        System.out.println(pizza.toppings); 
     }
 }
 
@@ -18,14 +18,14 @@ class Pizza {
     String cheese;
     String toppings;
 
-    Pizza(String bread, String sauce, String cheese, String toppings) {
+    Pizza(String bread, String sauce, String cheese, String toppings) { // constructor with 4 parameters
         this.bread = bread;
         this.sauce = sauce;
         this.cheese = cheese;
         this.toppings = toppings;
     }
 
-    Pizza(String bread, String sauce, String cheese) {
+    Pizza(String bread, String sauce, String cheese) { // constructor with 3 parameters
         this.bread = bread;
         this.sauce = sauce;
         this.cheese = cheese;
