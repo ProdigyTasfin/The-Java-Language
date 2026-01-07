d12b87cf66f1df315693b183d74f675ddf2baf1b
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-01-07 21:55:42 +0600

    OOP

---
+++ OOP_03_01.java
@@ -0,0 +1,23 @@
+public class OOP_03_01 {
+    public static void (String[] args){
+        Myself bio = new Myself();
+
+        System.out.println(bio.toString());
+
+    }
+}
+
+class Myself{
+
+    String Name = "Tasfin";
+    int age = 22;
+    double weight = 67.6;
+    String hobby = "Writing";
+
+    public String toString(){
+        String myString = "Name: " + Name + "\n"+ "Age: " + age + "\n" + "Weight: "+ weight + "\n" + "Hobby: "+ hobby;
+        return myString;
+    }
+
+
+}
