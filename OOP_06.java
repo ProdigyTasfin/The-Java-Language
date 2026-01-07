7a04a295dc411b71974734deb0c8437851b96aa3
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-01-08 02:27:14 +0600

    OOP

---
+++ OOP_06.java
@@ -0,0 +1,24 @@
+// static keyword
+
+public class OOP_06 {
+    public static void main(String[] args){
+
+    Friend friend1 = new Friend("Tasfin");
+    Friend friend2 = new Friend("Jessica");
+
+    System.out.println(Friend.numberOfFriends);
+
+    }
+}
+    class Friend{
+
+        String name;
+        static numberOfFriends;
+
+        Friend(String name){
+            this.name = name;
+            numberOfFriends++;
+        }
+
+        
+    }
