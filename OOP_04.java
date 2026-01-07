b2a8d4e8b3136135d248e7fc9a2c50663bb04108
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-01-08 01:48:32 +0600

    OOP

--- OOP_04.java
+++ OOP_04.java
@@ -8,9 +8,18 @@ public class OOP_04 {
         Food food1 = new Food("Burger");
         Food food2 = new Food("Pizza");
         Food food3 = new Food("Pasta");
-        refrigerator[0] = food1;
-        refrigerator[1] = food2;
-        refrigerator[2] = food3;
+
+        // method 01: manually assigning objects to array indices
+        // refrigerator[0] = food1;
+        // refrigerator[1] = food2;
+        // refrigerator[2] = food3;
+
+        //  method 02: using loop to assign objects to array indices
+        Food[] foods = {food1, food2, food3};
+        for (int i = 0; i < foods.length; i++) {
+            refrigerator[i] = foods[i];
+        }
+
 
         for (int i = 0; i < refrigerator.length; i++) {
             System.out.println("Memory Address: " + refrigerator[i]);
@@ -23,11 +32,11 @@ public class OOP_04 {
 
 class Food{
 
-    String name;
+    String name; //explanation name is important here because we are accessing it in the main method
 
     Food(String name){
         
-        this.name = name; 
+        this.name = name; // this.name refers to the instance variable, name refers to the parameter
 
     }
 
