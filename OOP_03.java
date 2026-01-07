a25c74a36213d926a8d3f2cd96333be49e9afbd9
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-01-07 21:34:07 +0600

    OOP

---
+++ OOP_03.java
@@ -0,0 +1,35 @@
+//toString 
+
+public class OOP_03 {
+    public static void main(String[] args){
+        
+        Car car = new Car();
+
+        // System.out.println(car.make);
+        // System.out.println(car.model);
+        // System.out.println(car.color);
+        // System.out.println(car.year);
+
+        System.out.println(car.toString());
+         
+    }
+
+}
+
+class Car {
+    String model = "Mustang";
+    String make = "Ford";
+    int year = 2024;
+    String color = "yellow";
+
+    public String toString(){
+        //1st method:
+        // String myString = make + "\n" + model + "\n" + color + "\n" + year;
+        // return myString;
+
+        // //2nd method: 
+        return make + "\n" + model + "\n" + color + "\n" + year;
+    }
+
+
+}
