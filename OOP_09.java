cc4e70cafb5be18d466fa3407caa9c0bdf3ac4db
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-01-08 22:44:19 +0600

    OOP

--- OOP_09.java
+++ OOP_09.java
@@ -7,11 +7,14 @@ public class OOP_09 {
     public static void main(String[] args) {
         
         Hero hero1 = new Hero("Batman", 26 , "He has a bat mobile");
+        Hero hero2 = new Hero("Thor" , 36, "Lightning");
         
         System.out.println("The Hero is: ");
         System.out.println(hero1.name);
         System.out.println(hero1.age);
         System.out.println(hero1.power);
+
+        System.out.println("The Hero is: " +"\n"+ "Name: " +hero2.name+"\n"+ "Age: " +hero2.age+"\n"+"Power: " +hero2.power+"\n");
     }
 }
 
