1915e2ae5171fade802ea1c2fd542177b290c055
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-01-08 22:48:48 +0600

    OOP

--- OOP_09.java
+++ OOP_09.java
@@ -3,18 +3,19 @@
 // "this" keyword
 
 public class OOP_09 {
-    
+
     public static void main(String[] args) {
-        
-        Hero hero1 = new Hero("Batman", 26 , "He has a bat mobile");
-        Hero hero2 = new Hero("Thor" , 36, "Lightning");
-        
+
+        Hero hero1 = new Hero("Batman", 26, "He has a bat mobile");
+        Hero hero2 = new Hero("Thor", 36, "Lightning");
+
         System.out.println("The Hero is: ");
         System.out.println(hero1.name);
         System.out.println(hero1.age);
         System.out.println(hero1.power);
 
-        System.out.println("The Hero is: " +"\n"+ "Name: " +hero2.name+"\n"+ "Age: " +hero2.age+"\n"+"Power: " +hero2.power+"\n");
+        System.out.println("The Hero is: " + "\n" + "Name: " + hero2.name + "\n" + "Age: " + hero2.age + "\n"
+                + "Power: " + hero2.power + "\n");
     }
 }
 
@@ -30,11 +31,11 @@ class Person {
     }
 }
 
-class Hero extends Person{
+class Hero extends Person {
 
     String power;
 
-    Hero (String name, int age, String power) {
+    Hero(String name, int age, String power) {
 
         super(name, age);
         this.power = power;
