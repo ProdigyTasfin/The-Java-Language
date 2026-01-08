645eeff2cd274035ac8a3198938c5fee06a25403
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-01-09 01:33:16 +0600

    OOP

---
+++ OOP_11.java
@@ -0,0 +1,23 @@
+
+// package 
+
+import Tools.Toolbox;
+
+public class OOP_11 {
+
+    public static void main(String[] args){
+
+        Toolbox toolbox = new Toolbox();
+
+    }
+    
+}
+
+
+
+public class Toolbox {
+
+    public Toolbox(){
+        System.out.println("Toolbox created!");
+    }
+}
