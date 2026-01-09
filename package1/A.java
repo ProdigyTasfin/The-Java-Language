d95a3f61b0fef7186cf9b104ff8203521925df19
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-01-09 23:09:14 +0600

    OOP

--- package1/A.java
+++ package1/A.java
@@ -3,9 +3,6 @@ import package2.*;
 
 public class A {
 
-    public static void main(String[] args) {
-        C c = new C();
-        System.out.println(c.publicMessage);
-    }
+    protected String protectedMessage = "This is a protected message.";
     
 }
--- package2/C.java
+++ package2/C.java
@@ -5,5 +5,6 @@ import package1.*;
 public class C {
     
     public String publicMessage = "This is a public message.";
+    protected String protectedMessage = "This is a protected message.";
     String defaultMessage = "This is the default message.";
 }
