83c5df2a14498d3f241da6cfd6ee409a6b7945e3
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-01-09 23:33:28 +0600

    OOP

--- OOP_13.java
+++ OOP_13.java
@@ -25,7 +25,7 @@ public class OOP_13 {
         System.out.println("Car Model: " + car.getModel());
         System.out.println("Car Year: " + car.getYear());
 
-        System.out.print("\n" + "Updating car year...");
+        System.out.print("\n" + "Updating car year..."+ "\n");
 
         car.setYear(2022); // updating year using setter method
 
