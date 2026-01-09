6e4160594257b19e908ff237b7223febc1d4bf24
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-01-09 23:10:57 +0600

    OOP

--- OOP_12.java
+++ OOP_12.java
@@ -1,5 +1,11 @@
 // Access modifier 
 
-public class OOP_12 {
-    
-}
+// check package 1 and package 2 folder
+
+// there are 4 access modifiers in java
+// 1. public
+// 2. private
+// 3. protected
+// 4. default (no modifier)
+
+
