f8e82fd5a722fbe2941cc33359fff3ea960376f9
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-01-10 15:53:59 +0600

    TRICKY QUESTION

--- TrickyQ1.java
+++ TrickyQ1.java
@@ -15,13 +15,13 @@ public class TrickyQ1 {
         String b = "java";
         String c = new String("java");
 
-        System.out.print(a + "\n");
-        System.out.print(b + "\n");
-        System.out.print(c + "\n");
+        // System.out.print(a + "\n");
+        // System.out.print(b + "\n");
+        // System.out.print(c + "\n");                   // Just checking values
 
-        System.out.println(System.identityHashCode(a));
-        System.out.println(System.identityHashCode(b));
-        System.out.println(System.identityHashCode(c));
+        // System.out.println(System.identityHashCode(a));
+        // System.out.println(System.identityHashCode(b));
+        // System.out.println(System.identityHashCode(c));
 
 
         System.out.println(a == b);
@@ -29,3 +29,21 @@ public class TrickyQ1 {
         System.out.println(a.equals(c));
     }
 }
+
+/*  OUTPUT :    java
+                java
+                java
+                1247233941
+                1247233941
+                258952499
+                true
+                false
+                true 
+
+                IT SEEMS SAME BUT DIFFERENT IN THE MEMORY ADDRESS! When a = java, b = java, storing 
+                same object which share same memory address
+
+                but when it equals to it focred to heap memory store it. So looking charactrer- wise
+                same but memory wise different. 
+
+*/
