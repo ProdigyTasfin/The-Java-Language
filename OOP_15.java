c60eb1ab3ff8a07be53d367ec5da09dfa6cb474b
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-01-11 22:26:18 +0600

    OOP

---
+++ OOP_15.java
@@ -0,0 +1,34 @@
+// interface = a template that can be applied to a class.
+//             similar to inheritance, but specifies what a class has/must do.
+//             classes can apply more than one interface, inheritance is limited to 1 super
+
+public class OOP_15{
+
+    public static void main(String[] args){
+
+        Fish fish = new fish();
+    }
+}
+
+interface Prey {
+
+    void flee();
+}
+
+interface Predator {
+
+}
+
+class Fish{
+
+}
+
+class Rabbit implements Prey{
+
+    @Override 
+
+    public void flee(){
+        System.out.println("This rabbit is fleeing");
+    }
+
+}
