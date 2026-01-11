69c12f365050af69209ee96dcdf5da7bf9bacbaa
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-01-12 00:34:53 +0600

    OOP

--- OOP_16.java
+++ OOP_16.java
@@ -5,21 +5,37 @@
 public class OOP_16{
     public static void main(String[] args){
 
+        Car car = new Car();
+        Bicycle bicycle = new Bicycle();
+        Boat boat = new Boat();
+
+        Vehicle[] racers = {car,bicycle,boat}; 
+
     }
 }
 
 class Vehicle { 
 
+
+
 }
 
 class Car extends Vehicle {
 
+    public void go() {
+        system.out.println("The car brgins moving");
+    }
 }
 
 class Bicycle extends Vehicle {
-    
+     public void go() {
+        system.out.println("The bicycle brgins moving");
+    }
 }
 
 class Boat extends Vehicle {
     
+     public void go() {
+        system.out.println("The boat brgins moving");
+    }
 }
