ce840099e1c521371fa153ea45c389b65b58c229
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-01-13 02:09:05 +0600

    OOP

--- OOP_17.java
+++ OOP_17.java
@@ -1,28 +1,35 @@
 
 // Dynamic Polymorphism 
+public class OOP_17 {
 
-public class OOP_17 { 
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Animal animal;
 
     }
-    
+
 }
-    class Animal{
 
+class Animal {
+
+    public void speak() {
+
+        System.out.println("Animal is sounding real :xD");
     }
 
-    class Dog extends Animal {
-        public void speak() {
-            System.out.println("Cat is mewoing");
-        }
+}
 
+class Dog extends Animal {
+
+    public void speak() {
+        System.out.println("Cat is mewoing");
     }
 
-     class Cat extends Animal {
-        public void speak(){
+}
+
+class Cat extends Animal {
+
+    public void speak() {
 
         System.out.println("Dog is barking");
 
