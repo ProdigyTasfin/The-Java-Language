83bf6ef2dc709671b07530b3d69b02a3b9019cea
ProdigyTasfin <tasfinhasansakib165@gmail.com>
2026-01-13 01:58:30 +0600

    OOP

--- OOP_17.java
+++ OOP_17.java
@@ -15,14 +15,16 @@ public class OOP_17 {
     }
 
     class Dog extends Animal {
-        public void speak(){
+        public void speak() {
             System.out.println("Cat is mewoing");
         }
 
     }
 
      class Cat extends Animal {
+        public void speak(){
 
         System.out.println("Dog is barking");
 
     }
+}
